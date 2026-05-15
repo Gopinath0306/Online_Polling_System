c4cc30daa18730be78fa719c8effc8e9efb9f58a
Gopinath0306 <gopinathvasuki907@gmail.com>
2026-05-15 16:59:41 +0530

    file modified

--- src/com/management/DBConnectionManager.java
+++ src/com/management/DBConnectionManager.java
@@ -1,5 +1,23 @@
 package com.management;
 
+import java.sql.Connection;
+
 public class DBConnectionManager {
 
+	private static String url="";
+	private static String password="123456789";
+	private static String user="root";
+	
+	public static void getConnection() {
+		
+	}
+	
 }
+
+//echo "# sample" >> README.md
+//git init
+//git add README.md
+//git commit -m "first commit"
+//git branch -M main
+//git remote add origin https://github.com/Gopinath0306/sample.git
+//git push -u origin main
