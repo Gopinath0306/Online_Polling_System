ff7d03851384b56442040d6b22cc3e194f59bd2e
balakrishna6374 <krishbala6374@gmail.com>
2026-05-18 11:06:52 +0530

    VoterService modified

--- src/com/service/VoterService.java
+++ src/com/service/VoterService.java
@@ -1,5 +1,25 @@
 package com.service;
+import java.util.*;
+
+import com.management.VoterManagement;
+import com.model.Voter;
 
 public class VoterService {
+	
+	public String generateVoterId() {
+		return "V" + UUID.randomUUID().toString().substring(0,5);
+	}
+	
+	public boolean validateAge(int age) {
+		return age >= 18;
+	}
+	
+	public boolean addVoter(Voter voter) {
+		if(!validateAge(voter.getAge())) {
+			System.out.println("Voter age must be 18 or above");
+			return false;
+		}
+		
+	}
 
 }
