050ffe940449790de785c298a5fa60a17f8da843
anuvetri2003 <anuvetri2003@gmail.com>
2026-05-18 16:19:21 +0530

    Update VoterManagement.java

--- src/com/management/VoterManagement.java
+++ src/com/management/VoterManagement.java
@@ -1,5 +1,64 @@
 package com.management;
-
-public class VoterManagement {
-
+import java.util.ArrayList;
+import java.util.List;
+import com.model.Voter;
+public class VoterManagement 
+{
+	private List<Voter> voterList = new ArrayList<>();
+	private Voter list;
+	public List<Voter> getVoterList() 
+	{
+		return voterList;
+	}
+	public void setVoterList(List<Voter> voterList) 
+	{
+		this.voterList = voterList;
+	}
+	public void insertVoter(List<Voter> voterList)
+	{
+			voterList.add(list);
+	}
+	public void updateVoter(String voterId, String address)
+	{
+		for(Voter v : voterList)
+		{
+			if(v.getVoterId().equals(voterId))
+			{
+				v.setAddress(address);
+			}
+		}
+	}
+	public boolean isVoterExists(String voterId)
+	{
+		for(Voter v : voterList)
+		{
+			if(v.getVoterId().equals(voterId))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	public Voter getVoterById(String voterId)
+	{
+		for(Voter v : voterList)
+		{
+			if(v.getVoterId().equals(voterId))
+			{
+				return v;
+			}
+		}
+		return null;
+	}
+	public void deleteVoter(String voterId)
+	{
+		for(int i = 0; i < voterList.size(); i++)
+		{
+			if(voterList.get(i).getVoterId().equals(voterId))
+			{
+				voterList.remove(i);
+				break;
+			}
+		}
+	}
 }
