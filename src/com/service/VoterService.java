d380cbcc103e46f030974dbcbcff051f286f5a48
balakrishna6374 <krishbala6374@gmail.com>
2026-05-18 12:42:23 +0530

    VoterService modified

--- src/com/service/VoterService.java
+++ src/com/service/VoterService.java
@@ -1,25 +1,68 @@
 package com.service;
-import java.util.*;
 
 import com.management.VoterManagement;
 import com.model.Voter;
 
 public class VoterService {
-	
-	public String generateVoterId() {
-		return "V" + UUID.randomUUID().toString().substring(0,5);
-	}
-	
-	public boolean validateAge(int age) {
-		return age >= 18;
-	}
-	
-	public boolean addVoter(Voter voter) {
-		if(!validateAge(voter.getAge())) {
-			System.out.println("Voter age must be 18 or above");
-			return false;
-		}
-		
-	}
 
+    VoterManagement management = new VoterManagement();
+
+    // Generate Voter ID
+    public String generateVoterId() {
+
+        return "V" + UUID.randomUUID().toString().substring(0,5);
+    }
+
+    // Validate Age
+    public boolean validateAge(int age) {
+
+        return age >= 18;
+    }
+
+    // Add Voter
+    public boolean addVoter(Voter voter) {
+
+        if(!validateAge(voter.getAge())) {
+
+            System.out.println("Voter age must be 18 or above");
+
+            return false;
+        }
+
+        // Generate Voter ID
+        voter.setVoterId(generateVoterId());
+
+        // Insert into Database
+        return management.insertVoter(voter);
+    }
+
+    // Search Voter By ID
+    public Voter searchVoter(String voterId) {
+
+        return management.getVoterById(voterId);
+    }
+
+    // Modify Address
+    public boolean modifyAddress(String voterId,String newAddress) {
+
+        return management.updateAddress(voterId,newAddress);
+    }
+
+    // Modify Mobile Number
+    public boolean modifyMobile(String voterId, long newMobile) {
+
+        return management.updateMobile(voterId,newMobile);
+    }
+
+    // Remove Voter
+    public boolean removeVoter(String voterId) {
+
+        return management.deleteVoter(voterId);
+    }
+
+    // Get District Wise Voters
+    public List<Voter> getDistrictWiseVoters(String district) {
+
+        return management.getVotersByDistrict(district);
+    }
 }
